5de9d20794286e3eeb340ace585d53690e41d474
eralpkor <eralp.ny@gmail.com>
2022-06-13 14:21:06 -0400

    Add auth-router and route

--- src/api/server.js
+++ src/api/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const authRouter = require("../auth/auth-router");
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use("/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.send("<h2>Let's cook something! 🌽🥕 😄</h2>");
