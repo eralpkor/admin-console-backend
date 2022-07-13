87701c12d8196552ba61a46be9fde7e4f369d232
eralpkor <eralp.ny@gmail.com>
2022-07-13 12:28:06 -0400

    Add customer inputs validation

---
+++ src/auth/validateCustomer.js
@@ -0,0 +1,48 @@
+const Customers = require("./auth-customer-model");
+
+module.exports = async (req, res, next) => {
+  const errors = [];
+
+  function validateCustomer(user) {
+    console.log(user);
+    !user.first_name && errors.push({ first_name: "required" });
+    !user.last_name && errors.push({ last_name: "required" });
+    !user.email && errors.push({ email: "required" });
+    // user.is_admin === 0 || user.is_admin === 1 && errors.push({ is_admin: "required" });
+
+    //Validate Char Length
+    Object.keys(user).map((x) => {
+      if (x === "first_name" || x === "last_name" || x === "email") {
+        const key = user[x].length;
+
+        //Verify Length Min
+        if (key < 2 && x) {
+          errors.push({ [x]: "Must be a minimum of 5 chars" });
+        }
+
+        //Verify Length Max
+        if (key > 50 && x) {
+          errors.push({ [x]: "Must be a maximum of 50 chars" });
+        }
+
+        //Validate Email Pattern
+        if (x === "email") {
+          !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user[x]) &&
+            errors.push({ error: "Unexpected Email Address" });
+        }
+      }
+    });
+  }
+
+  validateCustomer(req.body);
+
+  // Does user exist?
+  if (!errors.length) {
+    console.log("What is email ", req.body.email);
+    await Customers.findByEmail(req.body.email).then(
+      (email) => email && errors.push({ email: "Email Already Exist!" })
+    );
+  }
+  // OK we are probably safe to move on send conflict error
+  errors.length < 1 ? next() : res.status(409).json({ errors: errors });
+};
