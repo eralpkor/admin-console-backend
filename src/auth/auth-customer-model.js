1c62d110c9262cd23fc0f9ccd89e204c52376a95
eralpkor <eralp.ny@gmail.com>
2022-07-14 12:49:19 -0400

    Add update get edit models

--- src/auth/auth-customer-model.js
+++ src/auth/auth-customer-model.js
@@ -3,11 +3,15 @@ const db = require("../database/dbConfig");
 module.exports = {
   find,
   // addJob,
-  // findBy,
+  findBy,
   findByCustomerId,
   // findByUserId,
   // sortByFieldName,
   // findAllJobs,
+  updateCustomer,
+  getCustomer,
+  addCustomer,
+  findByEmail,
 };
 
 function find() {
@@ -16,4 +20,38 @@ function find() {
 
 function findByCustomerId(id) {
   return db("customers").where({ id }).first();
+}
+
+function updateCustomer(id, changed) {
+  return db("customers")
+    .where({ id })
+    .update(changed, ["id", "first_name", "last_name"]);
+}
+
+function getCustomer(id) {
+  return db("customers").select().where({ id }).first();
+}
+
+function addCustomer(params) {
+  return db("customers")
+    .insert(params, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findByCustomerId(id);
+    });
+}
+
+function findBy(filter) {
+  console.log(filter);
+  return db("customers").where(filter);
+}
+
+// for validation
+function findByEmail(email) {
+  return db("customers")
+    .select("email")
+    .where({
+      email,
+    })
+    .first();
 }
