43a4c67a567ec87ede86b8af56efe138708a735a
eralpkor <eralp.ny@gmail.com>
2022-07-21 11:49:25 -0400

    Add insert and update

--- src/auth/auth-accounts-model.js
+++ src/auth/auth-accounts-model.js
@@ -2,9 +2,9 @@ const db = require("../database/dbConfig");
 
 module.exports = {
   find,
-  // addOne,
+  addOne,
   findById,
-  // updateOne,
+  updateOne,
 };
 
 function find() {
@@ -16,9 +16,26 @@ function find() {
       "jobs.id as job_id"
     )
     .join("jobs", "jobs.id", "accounts.job_id")
-    .join("customers", "customers.id", "jobs.customer_id");
+    .join("customers", "customers.id", "jobs.customer_id")
+    .andWhere("accounts.is_deleted", false);
 }
 
 function findById(id) {
-  return db("accounts").select().where("account_id", id).first();
+  return db("accounts").select().where("job_id", id).first();
+}
+
+function updateOne(id, changes) {
+  return db("accounts").where({ id }).update(changes, "*");
+}
+
+function addOne(account) {
+  return db("accounts")
+    .select("accounts.*", "customers.first_name", "customers.last_name")
+    .join("jobs", "jobs.id", "accounts.job_id")
+    .join("customers", "customers.id", "jobs.customer_id")
+    .insert(account, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findById(id);
+    });
 }
