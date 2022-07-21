dd926f2892500aac917cd52cc977a62a231f3dd5
eralpkor <eralp.ny@gmail.com>
2022-07-21 19:34:52 -0400

    Add post route

--- src/auth/auth-accounts-router.js
+++ src/auth/auth-accounts-router.js
@@ -6,6 +6,7 @@ const Accounts = require("./auth-accounts-model");
 require("dotenv").config();
 
 function isObjectEmpty(obj) {
+  console.log(typeof obj);
   return Object.keys(obj).length === 0;
 }
 
@@ -59,6 +60,53 @@ router.get("/accounts/:id", (req, res) => {
       account
         ? res.status(200).json(account)
         : res.status(400).json({ message: "That account does not exist" });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Server error" });
+    });
+});
+
+router.post("/accounts", (req, res) => {
+  const account = req.body;
+  console.log(account);
+  if (isObjectEmpty(account)) {
+    return res.status(409).json({ error: "Please enter something" });
+  }
+  Accounts.updateOne(account)
+    .then((newAccount) => {
+      console.log(newAccount);
+      res.status(201).json(newAccount);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+router.put("/accounts/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  if (isObjectEmpty(changes)) {
+    return res.status(409).json({ error: "Please enter something" });
+  }
+
+  Accounts.findById(id)
+    .then((ids) => {
+      console.log(ids);
+      if (ids) {
+        Accounts.updateOne(id, changes)
+          .then((acc) => {
+            console.log(acc, changes);
+            res.status(200).json(changes);
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(404).json({ error: "No change happened..." });
+          });
+      } else {
+        res.status(404).json({ error: `No account with given id: ${id} ` });
+      }
     })
     .catch((error) => {
       console.log(error);
