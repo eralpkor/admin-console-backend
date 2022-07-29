4a3ba6da6feedb24837f5ce70d7b0f4cdd67c5fc
eralpkor <eralp.ny@gmail.com>
2022-07-29 11:31:12 -0400

    Fix json parse issue with filter

--- src/auth/auth-customer-router.js
+++ src/auth/auth-customer-router.js
@@ -2,13 +2,32 @@ const router = require("express").Router();
 require("dotenv").config();
 const Customers = require("./auth-customer-model");
 const validateCustomer = require("./validateCustomer");
+const Helpers = require("./middleware/helpers");
 
 // GET all customers
-router.get("/customers", (req, res) => {
+router.get("/customers", async (req, res) => {
+  let columnName, order, columnId, id;
+  if (req.query.sort) {
+    columnName = await JSON.parse(req.query.sort)[0];
+    order = await JSON.parse(req.query.sort)[1];
+  }
+  if (req.query.filter) {
+    columnId = await JSON.parse(req.query.filter);
+    if (columnId.id) {
+      id = columnId.id[0];
+    }
+  }
+
   Customers.find()
     .then((customers) => {
       res.setHeader(`Content-Range`, customers.length);
-
+      let sorted = customers;
+      if (order === "ASC") {
+        sorted = Helpers.sortAsc(customers, columnName);
+      }
+      if (order === "DESC") {
+        sorted = Helpers.sortDesc(customers, columnName);
+      }
       res.status(200).json(customers);
     })
     .catch((err) => {
