e9b31569f4d12abec4027234c5f08211643c1c7c
eralpkor <eralp.ny@gmail.com>
2022-08-01 11:56:47 -0400

    Seed file for roles

--- src/database/seeds/08_user_roles.js
+++ src/database/seeds/08_user_roles.js
@@ -4,13 +4,42 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("table_name").del();
-  await knex("table_name").insert([
+  await knex("user_roles").del();
+  await knex("user_roles").insert([
     {
+      user_role_id: 1,
       user_id: 1,
       role_id: 1,
     },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" },
+    {
+      user_role_id: 2,
+      user_id: 2,
+      role_id: 1,
+    },
+    {
+      user_role_id: 3,
+      user_id: 3,
+      role_id: 2,
+    },
+    {
+      user_role_id: 4,
+      user_id: 4,
+      role_id: 3,
+    },
+    {
+      user_role_id: 5,
+      user_id: 5,
+      role_id: 3,
+    },
+    {
+      user_role_id: 6,
+      user_id: 6,
+      role_id: 3,
+    },
+    {
+      user_role_id: 7,
+      user_id: 7,
+      role_id: 3,
+    },
   ]);
 };
