1f9322d98e05aba42c4b78f0cc00a7d6b7f69b84
eralpkor <eralp.ny@gmail.com>
2022-08-05 11:53:40 -0400

    Fix roles by table

--- src/auth/auth-model.js
+++ src/auth/auth-model.js
@@ -18,7 +18,8 @@ function find() {
   return db("users")
     .select("users.*", "roles.role")
     .join("user_roles", "user_roles.user_id", "users.id")
-    .join("roles", "roles.id", "user_roles.role_id");
+    .join("roles", "roles.id", "user_roles.role_id")
+    .where("is_deleted", false);
 }
 
 function getUser(id) {
@@ -31,21 +32,55 @@ function getUser(id) {
 }
 
 function addUser(user) {
-  return db("users")
-    .insert(user, "id")
-    .then((ids) => {
-      const [id] = ids;
+  return db
+    .transaction(function (t) {
+      console.log("whats user ", user);
+      return db("users")
+        .transacting(t)
+        .select()
+        .insert(
+          {
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name,
+          },
+          "id"
+        )
+        .then((response) => {
+          const [id] = response;
+          console.log("whats response", id);
+          return db("user_roles").transacting(t).select().insert(
+            {
+              role_id: user.role_id,
+              user_id: id,
+            },
+            "user_roles.user_id"
+          );
+        })
+        .then(t.commit)
+        .catch(t.rollback);
+    })
+    .then((response) => {
+      const [id] = response;
+      console.log("transaction succeeded ", id, response);
       return findById(id);
+    })
+    .catch((err) => {
+      console.log("transaction failed", err);
     });
 }
 
+// FIND user by id
 function findById(id) {
   return db("users")
-    .select("id", "username", "role_id", "first_name", "last_name")
+    .select("id", "username", "first_name", "last_name", "user_roles.role_id")
+    .join("user_roles", "users.id", "user_roles.user_id")
     .where({
       id,
     })
-    .first(); // return an object instead of an array
+    .first();
 }
 
 // filter-search function for login
@@ -59,23 +94,41 @@ function findBy(filter) {
 
 // Edit user info
 function editById(id, update) {
-  return db("users")
-    .where({ id })
-    .update(
-      {
-        username: update.username,
-        password: update.password,
-        email: update.email,
-        first_name: update.first_name,
-        last_name: update.last_name,
-        role_id: update.role_id,
-        updated_at: timestamp,
-      },
-      ["id", "username", "first_name"]
-    )
-    .then((ids) => {
-      console.log("whats ids ", ids, id);
+  return db
+    .transaction(function (t) {
+      return db("users")
+        .transacting(t)
+        .select()
+        .where({ id })
+        .update(
+          {
+            username: update.username,
+            password: update.password,
+            email: update.email,
+            first_name: update.first_name,
+            last_name: update.last_name,
+            updated_at: timestamp,
+          },
+          ["id", "username", "first_name"]
+        )
+        .then(() => {
+          return db("user_roles")
+            .transacting(t)
+            .select()
+            .where("user_id", id)
+            .update({
+              role_id: update.role_id,
+            });
+        })
+        .then(t.commit)
+        .catch(t.rollback);
+    })
+    .then(() => {
+      console.log("transaction succeeded ", id);
       return findById(id);
+    })
+    .catch((err) => {
+      console.log("transaction failed", err);
     });
 }
 
@@ -100,6 +153,6 @@ function findByEmail(email) {
 }
 
 // DELETE user
-function deleteOne(id, update) {
-  return db("users").where({ id }).del();
+function deleteOne(id) {
+  return db("users").where({ id }).update({ is_deleted: true });
 }
