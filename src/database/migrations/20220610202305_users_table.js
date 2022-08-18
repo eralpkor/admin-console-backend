9ac46dd19e0142212ee1a47873ccde47158ae168
eralpkor <eralp.ny@gmail.com>
2022-08-18 13:07:50 -0400

    Remove role_id

--- src/database/migrations/20220610202305_users_table.js
+++ src/database/migrations/20220610202305_users_table.js
@@ -11,7 +11,6 @@ exports.up = function (knex) {
     tbl.string("email", 128).unique();
     tbl.string("first_name", 50);
     tbl.string("last_name", 128);
-    tbl.integer("role_id");
     tbl.boolean("is_deleted").defaultTo(false); // set to true when deleted
     tbl.timestamp("created_at").defaultTo(knex.fn.now());
     tbl.timestamp("updated_at").defaultTo(knex.fn.now());
