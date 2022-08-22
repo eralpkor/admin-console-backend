abeedec4a8ab08496166663482f222f04ab3fc57
eralpkor <eralp.ny@gmail.com>
2022-08-22 19:47:58 -0400

    Add filter to get request

--- src/auth/auth-comments-router.js
+++ src/auth/auth-comments-router.js
@@ -10,42 +10,71 @@ require("dotenv").config();
 // http://localhost:5000/api/jobs?filter={"job_title":"investor"}&range=[0,24]&sort=["job_description","DESC"]
 // http://localhost:5000/api/jobs?filter={}&range=[0,24]&sort=["job_description","DESC"]
 router.get("/comments", async (req, res) => {
-  let columnName, order, columnId, id, startIndex, endIndex;
+  let result = [];
+  let columnName, order, startIndex, endIndex;
+
+  try {
+    result = await Comments.find();
+  } catch (error) {
+    res.status(500).json({ error: "Cannot get database..." });
+  }
 
   if (req.query.range) {
     startIndex = await JSON.parse(req.query.range)[0];
     endIndex = await JSON.parse(req.query.range)[1];
+    result = result.slice(startIndex, endIndex);
   }
   if (req.query.sort) {
     columnName = await JSON.parse(req.query.sort)[0];
     order = await JSON.parse(req.query.sort)[1];
-  }
-  if (req.query.filter) {
-    columnId = await JSON.parse(req.query.filter);
-
-    if (columnId.id) {
-      id = columnId.id[0];
+    if (order === "ASC") {
+      result = Helpers.sortAsc(result, columnName);
+    }
+    if (order === "DESC") {
+      result = Helpers.sortDesc(result, columnName);
     }
   }
 
-  Comments.find()
-    .then((comments) => {
-      res.setHeader(`Content-Range`, comments.length);
-      const result = comments.slice(startIndex, endIndex);
-      let sorted = result;
+  if (req.query.filter) {
+    let query = await JSON.parse(req.query.filter);
+    let { id } = await query;
 
-      if (order === "ASC") {
-        sorted = Helpers.sortAsc(result, columnName);
-      }
-      if (order === "DESC") {
-        sorted = Helpers.sortDesc(result, columnName);
-      }
-      res.status(200).json(sorted);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ error: "Cannot get comments" });
-    });
+    if (id) {
+      result = result.filter((x) => id.includes(x.job_id));
+    }
+    // if (search.job_title) {
+    //   let query = search.job_title.toLowerCase().trim();
+
+    //   result = result.filter((x) => {
+    //     let j = x.job_title.toLowerCase();
+    //     return j.includes(query);
+    //   });
+    // }
+    // if (search.assigned_to) {
+    //   let query = search.assigned_to.toLowerCase().trim();
+    //   console.log("query ", query);
+    //   result = result.filter((x) => {
+    //     let j = x.assigned_to.toLowerCase();
+    //     return j.includes(query);
+    //   });
+    // }
+    // if (search.last_name) {
+    //   let query = search.last_name.toLowerCase().trim();
+    //   result = result.filter((x) => {
+    //     let j = x.last_name.toLowerCase();
+    //     return j.includes(query);
+    //   });
+    // }
+    // if (search.in_progress) {
+    //   let query = search.in_progress.toLowerCase().trim();
+    //   result = result.filter((x) => {
+    //     let j = x.in_progress.toLowerCase();
+    //     return j.includes(query);
+    //   });
+    // }
+  }
+  res.setHeader(`Content-Range`, result.length);
+  res.status(200).json(result);
 });
 
 // comments by jobId
@@ -62,6 +91,48 @@ router.post("/comments", async (req, res) => {
     .catch((error) => {
       console.log("Comment POST error ", error);
       res.status(500).json("server error ", error);
+    });
+});
+
+// GET single comment
+router.get("/comments/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await Comments.findById(id);
+
+  try {
+    if (data) {
+      res.status(200).json({ ...data });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error ", error });
+  }
+});
+
+router.put("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (Object.keys(changes).length === 0) {
+    res.status(422).json({ error: "Request body cannot be empty." });
+  }
+  console.log("edit comments ", id);
+  Comments.findById(id)
+    .then((ids) => {
+      if (ids) {
+        Comments.updateOne(id, changes)
+          .then((data) => {
+            res.status(201).json(data);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(404).json({ error: "No change happened..." });
+          });
+      } else {
+        res.status(404).json({ message: `No comment with given id: ${id} ` });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Server error" });
     });
 });
 
