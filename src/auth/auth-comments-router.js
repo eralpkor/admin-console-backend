1e7f9941bc9529b6a516d6dfc6f463c2d70f19e4
eralpkor <eralp.ny@gmail.com>
2022-09-26 12:06:56 -0400

    Add try/catch block to conditional statements in get

--- src/auth/auth-comments-router.js
+++ src/auth/auth-comments-router.js
@@ -1,64 +1,70 @@
 const router = require("express").Router();
-const Comments = require("./auth-comments-model");
+const comment = require("./auth-comments-model");
 const Helpers = require("./middleware/helpers");
 require("dotenv").config();
 
-// All comments
-// http://localhost:5000/api/comments?filter={}&range=[10,19]&sort=["id","ASC"]
-// http://localhost:5000/api/comments?filter={}&range=[0,24]&sort=["id","ASC"]
+// All comment
+// http://localhost:5000/api/comment?filter={}&range=[10,19]&sort=["id","ASC"]
+// http://localhost:5000/api/comment?filter={}&range=[0,24]&sort=["id","ASC"]
 // http://localhost:5000/api/jobs?filter={"job_title":"investor"}&range=[0,24]&sort=["job_description","DESC"]
 // http://localhost:5000/api/jobs?filter={}&range=[0,24]&sort=["job_description","DESC"]
-router.get("/comments", async (req, res) => {
+router.get("/comment", async (req, res) => {
   let result = [];
   let columnName, order, startIndex, endIndex;
 
   try {
-    result = await Comments.find();
+    result = await comment.find();
   } catch (error) {
     res.status(500).json({ error: "Cannot get database..." });
   }
 
-  if (req.query.range) {
-    startIndex = await JSON.parse(req.query.range)[0];
-    endIndex = await JSON.parse(req.query.range)[1];
-    result = result.slice(startIndex, endIndex);
-  }
-  if (req.query.sort) {
-    columnName = await JSON.parse(req.query.sort)[0];
-    order = await JSON.parse(req.query.sort)[1];
-    if (order === "ASC") {
-      result = Helpers.sortAsc(result, columnName);
+  try {
+    if (req.query.range) {
+      startIndex = await JSON.parse(req.query.range)[0];
+      endIndex = await JSON.parse(req.query.range)[1];
+      result = result.slice(startIndex, endIndex);
     }
-    if (order === "DESC") {
-      result = Helpers.sortDesc(result, columnName);
+    if (req.query.sort) {
+      columnName = await JSON.parse(req.query.sort)[0];
+      order = await JSON.parse(req.query.sort)[1];
+      if (order === "ASC") {
+        result = Helpers.sortAsc(result, columnName);
+      }
+      if (order === "DESC") {
+        result = Helpers.sortDesc(result, columnName);
+      }
     }
-  }
 
-  if (req.query.filter) {
-    let search = await JSON.parse(req.query.filter);
+    if (req.query.filter) {
+      let search = await JSON.parse(req.query.filter);
+      let { id } = await search;
 
-    if (search.job_id) {
-      result = result.filter((x) => id.includes(x.job_id));
+      if (id) {
+        result = result.filter((x) => id.includes(x.jobId));
+      }
+      if (search.comment) {
+        let query = search.comment.toLowerCase().trim();
+        result = result.filter((x) => {
+          let c = x.comment.toLowerCase();
+          return c.includes(query);
+        });
+      }
     }
-    if (search.comment) {
-      let query = search.comment.toLowerCase().trim();
-      result = result.filter((x) => {
-        let c = x.comment.toLowerCase();
-        return c.includes(query);
-      });
-    }
+  } catch (error) {
+    console.log("Wrong JSON ", error);
   }
   res.setHeader(`Content-Range`, result.length);
   res.status(200).json(result);
 });
 
-// comments by jobId
-router.post("/comments", async (req, res) => {
+// comment by jobId
+router.post("/comment", async (req, res) => {
   const body = req.body;
   if (Helpers.isObjectEmpty(body))
     return res.status(409).json({ error: "Please enter something" });
 
-  Comments.addOne(body)
+  comment
+    .addOne(body)
     .then((comment) => {
       res.status(201).json(comment);
     })
@@ -69,9 +75,9 @@ router.post("/comments", async (req, res) => {
 });
 
 // GET single comment
-router.get("/comments/:id", async (req, res) => {
+router.get("/comment/:id", async (req, res) => {
   const { id } = req.params;
-  const data = await Comments.findById(id);
+  const data = await comment.findById(id);
 
   try {
     if (data) {
@@ -82,17 +88,19 @@ router.get("/comments/:id", async (req, res) => {
   }
 });
 
-router.put("/comments/:id", (req, res) => {
+router.put("/comment/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
   if (Object.keys(changes).length === 0) {
     res.status(422).json({ error: "Request body cannot be empty." });
   }
-  console.log("edit comments ", id, changes);
-  Comments.findById(id)
+  console.log("edit comment ", id, changes);
+  comment
+    .findById(id)
     .then((ids) => {
       if (ids) {
-        Comments.updateOne(id, changes)
+        comment
+          .updateOne(id, changes)
           .then((data) => {
             res.status(201).json(data);
           })
