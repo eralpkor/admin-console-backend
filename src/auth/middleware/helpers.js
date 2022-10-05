a1c0c32bc7e0baf02c5557fc4c4b5973d30b8702
eralpkor <eralp.ny@gmail.com>
2022-10-04 23:11:56 -0400

    Add validate date object helper

--- src/auth/middleware/helpers.js
+++ src/auth/middleware/helpers.js
@@ -2,7 +2,16 @@ module.exports = {
   isObjectEmpty,
   sortAsc,
   sortDesc,
+  // filterSearch,
 };
+
+function isValidDate(date) {
+  return (
+    date &&
+    Object.prototype.toString.call(date) === "[object Date]" &&
+    !isNaN(date)
+  );
+}
 
 function isObjectEmpty(obj) {
   return Object.keys(obj).length === 0;
@@ -11,6 +20,10 @@ function isObjectEmpty(obj) {
 function sortAsc(arr, columnName) {
   return arr.sort((a, b) => {
     if (typeof a[columnName] === "number") {
+      return a[columnName] - b[columnName];
+    }
+
+    if (isValidDate(a[columnName])) {
       return a[columnName] - b[columnName];
     } else {
       return a[columnName].localeCompare(b[columnName]);
@@ -22,8 +35,26 @@ function sortDesc(arr, columnName) {
   return arr.sort((a, b) => {
     if (typeof a[columnName] === "number") {
       return b[columnName] - a[columnName];
+    }
+    if (isValidDate(a[columnName])) {
+      return b[columnName] - a[columnName];
     } else {
       return b[columnName].localeCompare(a[columnName]);
     }
   });
 }
+// http://localhost:5000/api/jobs?filter={"job_title":"job 3"}&range=[0,9]&sort=["id","ASC"]
+// async function filterSearch(search, result) {
+//   search = await JSON.parse(search);
+
+//   // console.log("search ", search);
+//   if (search.job_title) {
+//     return result.filter((t) => {
+//       search = search.job_title.toLowerCase().trim();
+//       console.log("search ", search);
+//       t = t.job_title.toLowerCase().trim();
+//       console.log("result  ", t);
+//       return search.job_title.includes(t.job_title);
+//     });
+//   }
+// }
