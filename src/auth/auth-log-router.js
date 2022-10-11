41402de36934ef5364d0f2deab0ad5045f87a627
eralpkor <eralp.ny@gmail.com>
2022-10-11 12:50:11 -0400

    Fix content range issue with pagination

--- src/auth/auth-log-router.js
+++ src/auth/auth-log-router.js
@@ -6,14 +6,13 @@ require("dotenv").config();
 
 // Get all logs
 router.get("/log", async (req, res) => {
-  console.log("query ", req.query);
   const role = req.decodedToken.role;
   let result = [];
-  let data = [];
-  let columnName, order, limit, page;
+  let columnName, order, limit, page, contentRange;
 
   try {
     result = await Log.find();
+    contentRange = result.length;
   } catch (error) {
     res.status(500).json({ error: "Cannot get database..." });
   }
@@ -21,31 +20,9 @@ router.get("/log", async (req, res) => {
   try {
     if (req.query.range) {
       page = await JSON.parse(req.query.range)[0];
-      // endIndex = await JSON.parse(req.query.range)[1];
       limit = await JSON.parse(req.query.range)[1];
-      // result = result.slice(startIndex, endIndex);
-      console.log("page and limit ", page, limit);
-      // console.log("page ", page);
-      // console.log("limit ", limit);
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
-      console.log("start end ", startIndex, endIndex);
-      result = result.slice(page, limit);
-      // console.log(result);
 
-      if (endIndex < result.length) {
-        // page = page + 1;
-        // limit = limit;
-        console.log("page ", page);
-      }
-      if (startIndex > 0) {
-        page = page - 1;
-        console.log("page ", page);
-        console.log("linit 2 ", limit);
-      }
-      // console.log("whats indexes ", startIndex, endIndex);
-      // // result = result.slice(startIndex, endIndex);
-      // result = result.slice(endIndex, startIndex);
+      result = result.slice(page, limit);
     }
     if (req.query.sort) {
       columnName = await JSON.parse(req.query.sort)[0];
@@ -65,6 +42,7 @@ router.get("/log", async (req, res) => {
           return [query.jobId].includes(x.jobId);
         });
       }
+
       if (query.id) {
         result = result.filter((x) => {
           return query.id.includes(x.id);
@@ -74,10 +52,7 @@ router.get("/log", async (req, res) => {
   } catch (error) {
     console.log("Wrong JSON ", error);
   }
-  // res.setHeader(`Content-Range`, result.length);
-  // res.setHeader(`Content-Range`, `posts 0-9/100`);
-
-  res.setHeader(`Content-Range`, `${page - limit}/${data.length}`);
+  res.setHeader(`Content-Range`, contentRange);
   res.status(200).json(result);
 });
 
